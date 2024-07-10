42aef85212bec73567f019628d0de35594daaf57
Eray Hanoğlu <e.hanoglu@panates.com>
2024-07-10 09:41:43 +0300

    Fixed: Removed unnecessary "/test/tsconfig.json"

--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -11,7 +11,7 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       parserOptions: {
         ecmaVersion: 2022,
-        project: ['tsconfig.json', './test/tsconfig.json'],
+        project: ['tsconfig.json'],
         sourceType: 'module',
       },
     },
